7336eb4576255f359dbf3067ef0f2ce3b87b2724
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2023-12-09 23:41:03 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Create a dictionary with data
+data = {'Name': ['John', 'Emma', 'Peter', 'Lisa'],
+    'Age': [25, 28, 30, 35],
+    'City': ['New York', 'London', 'Paris', 'Tokyo']}
+
+# Create a dataframe
+df = pd.DataFrame(data)
+
+# Print the dataframe
+print(df)
