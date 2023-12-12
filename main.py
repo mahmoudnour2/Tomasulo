c50c2e4dc943a980072f4776d960e21ce4d875f1
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2023-12-12 02:30:39 +0200

    built not yet working support for branch,jump, and call

--- main.py
+++ main.py
@@ -62,7 +62,11 @@ reservation_station_to_instruction_map= {
 
 issue_index = 0
 clock = 1
-while not instruction_table.df["Write Result"].all():
+stall_execution=False
+branch_instr_index=None
+number_of_instructions=len(instruction_table.df)
+skipped_instructions=0
+while instruction_table.instructions_written+skipped_instructions<number_of_instructions:
     print("Clock cycle: ", clock)
     recently_removed_reservation_station=None
     branch=False
@@ -76,30 +80,101 @@ while not instruction_table.df["Write Result"].all():
     try:
         most_prior_instruction = min(corresponding_instructions_index)
         reservation_station_index=indices_of_potential_reservation_stations_to_write[corresponding_instructions_index.index(most_prior_instruction)]
-        branch=Reservation_stations[reservation_station_index].write_result()
+        branch,ret,call=Reservation_stations[reservation_station_index].write_result(issue_index)
+        if call or ret:
+            target_index=cdb.get_value()
+            #flush instructions after return (from issue_index to the current index)
+            #flush= clear corresponding reservation stations and register status and update the instruction table
+            #update the PC (issue_index) to the return instruction's PC + 1
+            cleared_reservation_stations=[]
+            #flush reservation stations
+            for station,instruction_no in reservation_station_to_instruction_map.items():
+                if instruction_no in range(issue_index,target_index):
+                    station.clear()
+                    cleared_reservation_stations.append(station)
+                    reservation_station_to_instruction_map[station]=None
+
+            for cleared in cleared_reservation_stations:
+                print("Cleared reservation station: ", cleared.name)
+            #flush instruction table
+            for issue_state in instruction_table.df["Issue"][issue_index:issue_index+1]:
+                issue_state=False
+            #remove references of flushed instructions from reservation_stations
+            for reservation_station in Reservation_stations:
+                reservation_station.clear_dependency(cleared_reservation_stations)
+            #flush register status (flushed after the references problem is solved, because we need the register status to clear the reservation stations dependencies)
+            for register,station in register_file.Register_status.items():
+                if station in cleared_reservation_stations:
+                    register_file.Register_status[register]=None
         if branch:
-            branch_reservation_station=cdb.get_reservation_station()
+            print("Branch instruction")
             branch_offset=cdb.get_value()
-            index_of_branch_instruction=reservation_station_to_instruction_map[branch_reservation_station]
-            #TODO: flush instructions after branch (from index_of_branch_instruction+1 to the current index)
-            #TODO: update the PC (issue_index) to the branch instruction's PC + branch_offset + 1
+            bne_station=Reservation_stations[8]
+            index_of_branch_instruction=reservation_station_to_instruction_map[bne_station]
+            target_index=index_of_branch_instruction+branch_offset+1
+            skipped_instructions+=(target_index-issue_index-1)
+
+            cleared_reservation_stations=[]
+            #flush instructions after branch (from index_of_branch_instruction+1 to the current index)
+            #flush= clear corresponding reservation stations and register status and update the instruction table
+            #update the PC (issue_index) to the branch instruction's PC + branch_offset + 1
+            
+            #flush reservation stations
+            for station,instruction_no in reservation_station_to_instruction_map.items():
+                if instruction_no in range(index_of_branch_instruction+1,issue_index):
+                    station.clear()
+                    cleared_reservation_stations.append(station)
+                    reservation_station_to_instruction_map[station]=None
+
+            for cleared in cleared_reservation_stations:
+                print("Cleared reservation station: ", cleared.name)
+            #flush instruction table
+            for issue_state in instruction_table.df["Issue"][index_of_branch_instruction:issue_index]:
+                issue_state=False
+            #remove references of flushed instructions from reservation_stations
+            for reservation_station in Reservation_stations:
+                reservation_station.clear_dependency(cleared_reservation_stations)
+            #flush register status (flushed after the references problem is solved, because we need the register status to clear the reservation stations dependencies)
+            for register,station in register_file.Register_status.items():
+                if station in cleared_reservation_stations:
+                    register_file.Register_status[register]=None
+
+            issue_index=target_index#update the PC
+            instruction_table.issue_index=issue_index
+            stall_execution=False
+            print("End stall execution")
         recently_removed_reservation_station=Reservation_stations[reservation_station_index]
         instruction_table.write_result(most_prior_instruction)#update the instruction table
         #we update the instruction table after the issue so that we don't 
         # empty a reservation_unit and fill it with another function in the same cycle
-    except :
-        pass
+    except Exception as e:
+        # Print the exception details along with the line number
+        line_number = sys.exc_info()[-1].tb_lineno
+        print(f"An error occurred on line {line_number}: {type(e).__name__} - {str(e)}")
     #execute instruction if possible
     #TODO: if the instruction being executed is a branch, stop executing the rest of the instructions
-    for i in range(len(Reservation_stations)):
-        executed=Reservation_stations[i].execute()
-        if(executed):
-            instruction_table.execute(reservation_station_to_instruction_map[Reservation_stations[i]])#update the instruction table
-
+    if stall_execution==False:
+        for i in range(len(Reservation_stations)):
+            executed=Reservation_stations[i].execute()
+            if(executed):
+                instruction_table.execute(reservation_station_to_instruction_map[Reservation_stations[i]])#update the instruction table
+    else:
+        for station in Reservation_stations:
+            #execute instructions before the branch instruction only
+            if reservation_station_to_instruction_map[station]!=None:
+                if reservation_station_to_instruction_map[station]<=branch_instr_index:
+                    executed=station.execute()
+                    if(executed):
+                        instruction_table.execute(reservation_station_to_instruction_map[station])
+        
     #issue instruction if possible
     if issue_index<len(instruction_table.df):#check if there are any instructions left to issue 
-        print("Issue index: ", issue_index)
         instruction_type=instruction_table.df.loc[issue_index,"Operation"]
+        #stall execution of other instructions if the instruction issue is a branch
+        if instruction_type=="BNE":
+            branch_instr_index=issue_index
+            stall_execution=True
+            print("Start stall execution")
         suitable_reservation_stations=functional_units[instruction_type]
         the_available_unit_index=None
         for i in range(len(suitable_reservation_stations)):
@@ -121,7 +196,10 @@ while not instruction_table.df["Write Result"].all():
     register_file.check_data_bus()
     instruction_table.print_table()
     register_file.print_table()
+    print("In clock cycle: ", clock, "the stall execution is: ", stall_execution)
+    print("The branch instruction index is: ", branch_instr_index)
+    print("\n")
     clock+=1
-    if(clock==30):
+    if(clock==100):
         break
 register_file.print_table()
